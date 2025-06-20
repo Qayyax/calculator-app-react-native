867a69339b8f9c2464c9dde9526e48ee1a995513
Qayyax <106759130+Qayyax@users.noreply.github.com>
2025-06-20 14:58:07 -0400

    todo comment

--- util/calculation.ts
+++ util/calculation.ts
@@ -1,16 +1,5 @@
-// would export varios calculation logic
-// it should take an array
-// check for the expression, 
-// or better still, it would be a loop, 
-// when it gets * or any other expression, 
-// it should solve, something like that, 
-// let string = ""
-// string += value
-// if expression
-// string before expression <expression> string after expression <expre> string
-// solve
-
 const calculateExpression = (expression: string): string => {
+  // TODO: work on +/- to have ()
   if (/\d$/.test(expression) || /%$/.test(expression)) {
     let newExpression = expression.replaceAll("\u{00D7}", "*");
     newExpression = newExpression.replaceAll("\u{00F7}", "/")
